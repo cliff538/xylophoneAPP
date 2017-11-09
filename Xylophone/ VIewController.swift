c9848648cdb63e02e292910979d6263ada1561e2
cliff538 <cliff538@yahoo.com>
2017-11-09 07:18:31 -0700

    Sounds working for each key.

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,7 +12,11 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
+	// MARK: - Properties
 	var audioPlayer : AVAudioPlayer!
+	let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+	var selectedSoundFileName = ""
+	
 	
 	// MARK: - View Did Load
     override func viewDidLoad() {
@@ -20,18 +24,26 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
 
 
-
+ // MARK: - Interactions
+	@IBAction func notePressed(_ sender: UIButton) {
+		
+		selectedSoundFileName = soundArray[sender.tag - 1]
+		
+		playSound()
+	}
+	
+	
 	// MARK: - Functions
 	
 	//all 7 keys are under one @IBAction.Differentiated by using Tags, 1-7.
 	//if not using tags you'd have to make 7 diff. @IBAction's.
-	@IBAction func notePressed(_ sender: UIButton) {
+	func playSound() {
+		//testing: print tag numbers when buttons pressed.
+		//print(sender.tag)
 		
-		//test tag numbers when buttons pressed. 
-      //print(sender.tag)
+		let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
 		
-		let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
-		
+		//"do-catch-try" error handling.
 		do {
 			audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
 		}
@@ -40,11 +52,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 		}
 		
 		audioPlayer.play()
-
-        
-    }
-	
-	
+	}
 
 }
 
